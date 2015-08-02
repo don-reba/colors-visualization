ce4cfb837f9b75f18e18b0236b68d1f70472859f
don-reba <don_reba@inbox.ru>
2015-08-02 21:18:24 +0200

    Improved voxelization. Removed trilinear interpolation from rendering. Updated ConvertColor.dll to the latest Nemerle.

--- render/Render/Volume.cpp
+++ render/Render/Volume.cpp
@@ -20,15 +20,15 @@ namespace
 	{
 		stream.read(reinterpret_cast<char*>(data), sizeof(T));
 	}
-}
 
-//--------------------------------------
-// lab ranges
-//--------------------------------------
-const float minX( 0.0f  ), maxX(100.0f);
-const float minY(-86.0f ), maxY(99.0f );
-const float minZ(-108.0f), maxZ(95.0f );
-//--------------------------------------
+	//--------------------------------------
+	// lab ranges
+	//--------------------------------------
+	const float minX( 0.0f  ), maxX(100.0f);
+	const float minY(-86.0f ), maxY(99.0f );
+	const float minZ(-108.0f), maxZ(95.0f );
+	//--------------------------------------
+}
 
 Volume::Volume(size_t nx, size_t ny, size_t nz)
 	: Nx(nx), Ny(ny), Nz(nz)
@@ -61,54 +61,12 @@ float Volume::operator [] (const Vector3f & p) const
 	const int y((int)(fy));
 	const int z((int)(fz));
 
-	if (x < 0 || x > Nx - 1) return 0.0f;
-	if (y < 0 || y > Ny - 1) return 0.0f;
-	if (z < 0 || z > Nz - 1) return 0.0f;
+	if (x < 0 || x >= Nx) return 0.0f;
+	if (y < 0 || y >= Ny) return 0.0f;
+	if (z < 0 || z >= Nz) return 0.0f;
 
 	return Values[x + Nx * y + Nx * Ny * z];
 }
-
-/*
-float Volume::operator [] (const Vector3f & p) const
-{
-	// trlinear interpolation
-
-	const float fx(xFactor * (p.x() - minX));
-	const float fy(yFactor * (p.y() - minY));
-	const float fz(zFactor * (p.z() - minZ));
-
-	const int x((int)(fx));
-	const int y((int)(fy));
-	const int z((int)(fz));
-
-	if (x < 0 || x > Nx - 2) return 0.0f;
-	if (y < 0 || y > Ny - 2) return 0.0f;
-	if (z < 0 || z > Nz - 2) return 0.0f;
-
-	const float dx(fx - floor(fx));
-	const float dy(fy - floor(fy));
-	const float dz(fz - floor(fz));
-
-	const float v000(Values[(x + 0) + Nx * (y + 0) + Nx * Ny * (z + 0)]);
-	const float v001(Values[(x + 0) + Nx * (y + 0) + Nx * Ny * (z + 1)]);
-	const float v010(Values[(x + 0) + Nx * (y + 1) + Nx * Ny * (z + 0)]);
-	const float v011(Values[(x + 0) + Nx * (y + 1) + Nx * Ny * (z + 1)]);
-	const float v100(Values[(x + 1) + Nx * (y + 0) + Nx * Ny * (z + 0)]);
-	const float v101(Values[(x + 1) + Nx * (y + 0) + Nx * Ny * (z + 1)]);
-	const float v110(Values[(x + 1) + Nx * (y + 1) + Nx * Ny * (z + 0)]);
-	const float v111(Values[(x + 1) + Nx * (y + 1) + Nx * Ny * (z + 1)]);
-
-	const float v00((1.0f - dx) * v000 + dx * v001);
-	const float v01((1.0f - dx) * v010 + dx * v011);
-	const float v10((1.0f - dx) * v100 + dx * v101);
-	const float v11((1.0f - dx) * v110 + dx * v111);
-
-	const float v0((1.0f - dy) * v00 + dy * v01);
-	const float v1((1.0f - dy) * v10 + dy * v11);
-
-	return (1.0f - dz) * v0 + dz * v1;
-}
-*/
 
 Volume Volume::MakeTest()
 {
