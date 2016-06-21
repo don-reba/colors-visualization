033421cc2cf6c3c1fb596df9b88028247f2aeb9e
don-reba <don_reba@inbox.ru>
2016-06-21 19:48:09 +0200

    Modified the script to export data. Updated graphs.

--- fgt/graphs.r
+++ fgt/graphs.r
@@ -1,8 +1,11 @@
 report <- read.delim("C:/Users/Alexey/Projects/Colours visualization/fgt/report.tsv")
-report$error <- abs(report$exact - report$approximate)
+report$error     <- abs(report$exact - report$approximate)
+report$rel.error <- abs(report$exact - report$approximate) / report$exact
 
 qplot(alpha, error, data=report, geom="jitter", color=I(alpha("black", 1/5)))
 
 errors <- aggregate(error ~ alpha + n, data=report, FUN="mean")
 
 qplot(n, error, data=errors, facets=.~alpha)
+
+qplot(sigma, rel.error, data=report[report$alpha==5 & report$rel.error < 2,], color=I(alpha("black", 1/5)), facets=.~n)
